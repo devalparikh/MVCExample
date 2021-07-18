9bf245c0545d5547db178e63c25dd7bfde37f705
Deval Parikh <devalpp@gmail.com>
2021-07-18 01:59:33 -0400

    put endpoint

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 // Service Layer
@@ -23,20 +25,51 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
-        if(studentOptional.isPresent()) {
-            throw new IllegalStateException("email taken");
-        }
+        checkIfEmailIsTaken(student.getEmail());
+        // Instantiates a new student object
         studentRepository.save(student);
     }
 
     public void deleteStudent(Long studentId) {
-        boolean exists = studentRepository.existsById(studentId);
-        if(!exists) {
-            throw new IllegalStateException(
-                    "student with id " + studentId + "does not exist."
-            );
-        }
+        getExistingStudentById(studentId);
         studentRepository.deleteById(studentId);
     }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = getExistingStudentById(studentId);
+        if(name != null && name.length() > 0 && !Objects.equals((student.getName()), name)) {
+            // allows duplicate names
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals((student.getEmail()), email)) {
+            // does not allow duplicate emails
+            checkIfEmailIsTaken(email);
+            student.setEmail(email);
+        }
+    }
+
+    /**
+     * Checks if student exists by Id
+     * @param studentId
+     * @return student object if existing, else throws exception
+     */
+    private Student getExistingStudentById(Long studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist"));
+        return student;
+    }
+
+    /**
+     * Checks if email is taken by another student
+     * @param email
+     */
+    private void checkIfEmailIsTaken(String email) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+    }
+
 }
